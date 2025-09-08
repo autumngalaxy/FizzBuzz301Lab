e54817f00ba35ff1a520ab853de57f7221c7473f
autumngalaxy <39389365+autumngalaxy@users.noreply.github.com>
2025-09-08 10:36:48 -0400

    Overloaded multiples

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,7 +2,7 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        System.out.println(multiples(1000, 3, 5));
+        System.out.println(multiples());
     }
 
     public static int multiples(int n, int a, int b) {
@@ -15,5 +15,9 @@ public class Multiples {
             }
         }
         return multiples;
+    }
+    public static int multiples()
+    {
+        return multiples(1000, 3, 5);
     }
 }
